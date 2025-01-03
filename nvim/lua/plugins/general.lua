1212897679186b9589e9712e846584a4cc916126
Oleksandr Lynnyk <linnik.alexandr2@gmail.com>
2025-01-03 23:49:27 +0200

    add status bar, key description, error desc

--- nvim/lua/plugins/general.lua
+++ nvim/lua/plugins/general.lua
@@ -17,5 +17,29 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     opts = {}
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {}
+  },
+  {
+    "folke/trouble.nvim",
+    opts = {},
+    cmd = "Trouble"
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      {
+	"<leader>?",
+	function()
+	  require("which-key").show({ global = false })
+	end,
+	desc = "Buffer Local Keymaps (which-key)",
+      },
+    }
   }
 }
